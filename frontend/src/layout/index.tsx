1ddd0aa1b32e4bbbda794b504b81b3d64419858f
shuvo-99 <anontoshuvo99@gmail.com>
2024-02-21 14:25:07 +0600

    side navbar setting

--- frontend/src/layout/index.tsx
+++ frontend/src/layout/index.tsx
@@ -1,18 +1,27 @@
 
 import Navbar from "./navbar.tsx";
 import {Outlet} from "react-router-dom";
-import Grid from "@mui/material/Grid";
+// import Grid from "@mui/material/Grid";
 
 const Home = () => {
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={2} sx={{ backgroundColor : '#002E30'}}>
+        // <Grid container spacing={2}>
+        //     <Grid item xs={2} sx={{ backgroundColor : '#002E30', maxHeight:'100%'}}>
+        //         <Navbar></Navbar>
+        //     </Grid>
+        //     <Grid item xs={10}>
+        //         <Outlet></Outlet>
+        //     </Grid>
+        // </Grid>
+        <div style={{display: 'flex', height: '100vh'}}>
+            <div style={{backgroundColor: '#002E30', width: '20%'}}>
                 <Navbar></Navbar>
-            </Grid>
-            <Grid item xs={10}>
+            </div>
+            <div style={{flex: 1}}>
                 <Outlet></Outlet>
-            </Grid>
-        </Grid>
+            </div>
+        </div>
+
     );
 };
 
--- frontend/src/layout/navbar.tsx
+++ frontend/src/layout/navbar.tsx
@@ -1,34 +1,19 @@
 
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 
 const Navbar = () => {
+
+    const pages = ['Dashboard', 'Employees', 'Team Management', 'My Calender', 'Attendance', 'Payroll', 'User Management', 'Setting']
+    const links = ['/','/employees','/teamManagement','/myCalender','/attendance','/payroll','/userManagement','/setting']
+
     return (
         <div >
-            <ul style={{ listStyleType: 'none' }}>
-                <li>
-                    <Link style={{textDecoration:'None', color:'white'}} to='/'>Dashboard</Link>
-                </li>
-                <li>
-                    <Link style={{textDecoration:'None', color:'white'}} to='/employees'>Employees</Link>
-                </li>
-                <li>
-                    <Link style={{textDecoration:'None', color:'white'}} to='/teamManagement'>Team Management</Link>
-                </li>
-                <li>
-                    <Link style={{textDecoration:'None', color:'white'}} to='/myCalender'>My Calender</Link>
-                </li>
-                <li>
-                    <Link style={{textDecoration:'None', color:'white'}} to='/attendance'>Attendance</Link>
-                </li>
-                <li>
-                    <Link style={{textDecoration:'None', color:'white'}} to='/payroll'>Payroll</Link>
-                </li>
-                <li>
-                    <Link style={{textDecoration:'None', color:'white'}} to='/userManagement'>User Management</Link>
-                </li>
-                <li>
-                    <Link style={{textDecoration:'None', color:'white'}} to='/setting'>Setting</Link>
-                </li>
+            <ul style={{ listStyleType: 'none',}}>
+                {pages.map((page, i) => (
+                    <li key={i} style={{ marginTop: '10%', marginBottom: '10%' }}>
+                        <Link style={{ textDecoration: 'None', color: 'white' }} to={links[i]}>{page}</Link>
+                    </li>
+                ))}
             </ul>
         </div>
     );
